8629eb5e752c96357aa3dee70d81f37c2dec6253
tongaks <rrfrokuu@gmail.com>
2025-05-13 08:21:16 +0800

    added page focus (remove edit-element-menu when the page is clicked)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 document.addEventListener('contextmenu', (e)=>e.preventDefault());
 
 function pageFocus() {
+	let edit_menus = document.getElementsByClassName('edit-element-menu'); 
+	for (let i = 0; i < edit_menus.length; i++) {
+		edit_menus[i].remove();
+	}
+
 	// console.log('page focus');
 }
